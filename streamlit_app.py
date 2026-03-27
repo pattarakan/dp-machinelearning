e31800fd39a232786542c7f2dd5c03cb385b1c06
pattarakan <43806873+pattarakan@users.noreply.github.com>
2026-03-27 10:30:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Offshore Wind Farm Dashboard",
+    page_icon="🌊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 st.title('Machine learning')
 
 st.write('Machine Learning Model')
